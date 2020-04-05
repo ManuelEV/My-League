0ffb9699aad3e60e2243404de88028d682063bee
Manuel <manuelespv@gmail.com>
2020-04-05 19:01:36 -0400

    Controllers organization

--- Laravel-backend/app/Http/Controllers/Api/UserController.php
+++ Laravel-backend/app/Http/Controllers/Api/UserController.php
@@ -17,6 +17,7 @@ class UserController extends Controller
 	public function login () {
 		if (auth()->attempt(request()->input())) {
 			$user = auth()->user();
+			Log::info($user);
 			$success['token'] =  $user
 				->createToken('Passport Api', ['show-products'])
 				->accessToken;
